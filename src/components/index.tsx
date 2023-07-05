86f37da0d03ac298b3c9cb69ee10c8af81d55287
Emelie Barrington <emelie.barrington@chasacademy.se>
2023-07-05 13:05:56 +0200

    Code refactored to more reusable components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
 export { default as Button } from "./elements/Button";
-export { default as ShowRepo } from "./elements/ShowRepo";
 export { default as Homepage } from "./layouts/Homepage";
+export { default as ErrorCard } from "./elements/ErrorCard";
+export { default as RepoCard } from "./elements/RepoCard";
+
